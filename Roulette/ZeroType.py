db925a76642a2ecf122ae66f4ba77313055b733d
juannavascalvente <juan.navas.calvente@gmail.com>
2024-01-15 21:31:32 +0100

    Add more functions

--- Roulette/ZeroType.py
+++ Roulette/ZeroType.py
@@ -6,3 +6,11 @@ class ZeroType:
     @staticmethod
     def is_zero_or_double(value: int) -> bool:
         return value == ZeroType.ZERO or value == ZeroType.DOUBLE_ZERO
+
+    @staticmethod
+    def is_zero(value: int) -> bool:
+        return value == ZeroType.ZERO
+
+    @staticmethod
+    def is_zero_double(value: int) -> bool:
+        return value == ZeroType.DOUBLE_ZERO
